c56ee6e431f019d35c14e8a8a11473902b7ffbb4
Almas Khan <almas.rbm@gmail.com>
2020-09-04 03:50:29 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', 'LoginController@index')->name('login.index');
+Route::post('/login', 'LoginController@verify')->name('login.verify');
+Route::get('/logout', 'LogoutController@index')->name('logout.index');
+
+Route::group(['middleware'=>['sess']], function(){
+    // ADMIN
+	Route::get('/admin', 'AdminController@index')->name('admin.index');
+    Route::get('/admin/employeeList', 'EmployeeController@employeeList')->name('admin.employeeList');
+    Route::get('/admin/addEmployee', 'EmployeeController@create')->name('admin.create');
+    Route::post('/admin/addEmployee', 'EmployeeController@store')->name('admin.store');
+	Route::get('/admin/employee/details/{id}', 'EmployeeController@show')->name('admin.Empdetails');
+    Route::get('/admin/employee/delete/{id}', 'EmployeeController@delete')->name('admin.delete');
+    Route::post('/admin/employee/delete/{id}', 'EmployeeController@destroy')->name('admin.destroy');
+
+    // Employee
+    Route::get('/employee', 'EmployeeController@index')->name('employee.index');
+    Route::get('/employee/addJob', 'JobController@create')->name('employee.create');
+    Route::post('/employee/addJob', 'JobController@store')->name('employee.store');
+    Route::get('/employee/jobList', 'JobController@jobList')->name('employee.jobList');
+    Route::get('/employee/job/details/{id}', 'JobController@show')->name('employee.jobdetails');
+    Route::get('/employee/job/delete/{id}', 'JobController@delete')->name('employee.delete');
+    Route::post('/employee/job/delete/{id}', 'JobController@destroy')->name('employee.destroy');
+});
